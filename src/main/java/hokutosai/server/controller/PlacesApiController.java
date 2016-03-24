a5eda00704f647e5caccc30942af20646cf2213a
sejour <shuka.takakuma@gmail.com>
2016-03-25 01:01:47 +0900

    [implement] GET places/list
    場所情報の一覧を表すエンドポイントを実装

--- src/main/java/hokutosai/server/controller/PlacesApiController.java
+++ src/main/java/hokutosai/server/controller/PlacesApiController.java
@@ -1,14 +1,25 @@
 package hokutosai.server.controller;
 
+import hokutosai.server.data.entity.Place;
+import hokutosai.server.data.repository.PlaceRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
-@RequestMapping("/api")
+@RequestMapping("/api/places")
 public class PlacesApiController {
 
+	@Autowired
+    private PlaceRepository placeRepository;
 
+	 @RequestMapping(value = "/list", method = RequestMethod.GET)
+	 public Iterable<Place> list() {
+	     return placeRepository.findAll();
+	 }
 
 }
