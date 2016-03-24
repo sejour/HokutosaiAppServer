c9d58be547ae683fee1af5f9cffe538640c1cf1a
sejour <shuka.takakuma@gmail.com>
2016-03-24 23:49:29 +0900

    [implement] Place entity
    場所情報を表すためにPlaceを実装

---
+++ src/main/java/hokutosai/server/data/entity/Place.java
@@ -0,0 +1,26 @@
+package hokutosai.server.data.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Entity
+@Table(name = "places")
+@Data
+public class Place {
+
+	@Column(name = "place_id")
+	@Id
+	@JsonProperty("place_id")
+	private Integer placeId;
+
+	@Column(name = "name")
+	@JsonProperty("name")
+	private String name;
+
+}
