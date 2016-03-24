76f5cd553df2e6253e496e1e27585fcb2338a31d
sejour <shuka.takakuma@gmail.com>
2016-03-24 09:29:38 +0900

    [add] HokutosaiApiController.java
    HokutosaiAPIのUtilityAPIを実装するために作成

---
+++ src/main/java/hokutosai/server/controller/HokutosaiApiController.java
@@ -0,0 +1,12 @@
+package hokutosai.server.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@EnableAutoConfiguration
+@RequestMapping("/api")
+public class HokutosaiApiController {
+
+}
