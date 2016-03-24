8d783b69144f65d89bae7039adef3c326b421951
sejour <shuka.takakuma@gmail.com>
2016-03-24 09:51:07 +0900

    [add] method hello
    動作確認のために追加

--- src/main/java/hokutosai/server/controller/HokutosaiApiController.java
+++ src/main/java/hokutosai/server/controller/HokutosaiApiController.java
@@ -3,10 +3,17 @@ package hokutosai.server.controller;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @EnableAutoConfiguration
 @RequestMapping("/api")
 public class HokutosaiApiController {
+
+	 @RequestMapping("/hello")
+	 @ResponseBody
+	 public String hello() {
+	     return "Hello HokutosaiAPI";
+	 }
 
 }
