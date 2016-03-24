3762cf9d0ad180514d4ef3015142f8c731a3e481
sejour <shuka.takakuma@gmail.com>
2016-03-25 02:57:56 +0900

    [implement] GET shops/list
    模擬店の一覧をGETするエンドポイントを実装

--- src/main/java/hokutosai/server/controller/ShopsApiController.java
+++ src/main/java/hokutosai/server/controller/ShopsApiController.java
@@ -1,12 +1,25 @@
 package hokutosai.server.controller;
 
+import hokutosai.server.data.entity.Shop;
+import hokutosai.server.data.repository.ShopRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
 @RequestMapping("/api/shops")
 public class ShopsApiController {
+
+	@Autowired
+    private ShopRepository shopRepository;
+
+	 @RequestMapping(value = "/list", method = RequestMethod.GET)
+	 public Iterable<Shop> list() {
+	     return shopRepository.findAll();
+	 }
 
 }
