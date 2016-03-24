5eb07b67b7b92726d90c93dd719697964180fed8
sejour <shuka.takakuma@gmail.com>
2016-03-25 02:04:25 +0900

    [add] Shop entity

---
+++ src/main/java/hokutosai/server/data/entity/Shop.java
@@ -0,0 +1,13 @@
+package hokutosai.server.data.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "shops")
+@Data
+public class Shop {
+
+}
