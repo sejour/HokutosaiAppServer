67d3b546eb22199e3b0954867bac31185345fffc
sejour <shuka.takakuma@gmail.com>
2016-03-25 08:29:26 +0900

    [add] AssessedScore abstract entity
    評価スコアの抽象エンティティを定義

---
+++ src/main/java/hokutosai/server/data/entity/AssessedScore.java
@@ -0,0 +1,27 @@
+package hokutosai.server.data.entity;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@MappedSuperclass
+@Data
+public abstract class AssessedScore {
+
+	@Column(name = "shop_id")
+	@JsonIgnore
+	private Integer shopId;
+
+	@Column(name = "total_score")
+	@JsonProperty("total_score")
+	private Long totalScore;
+
+	@Column(name = "assessed_count")
+	@JsonProperty("assessed_count")
+	private Long assessdCount;
+
+}
