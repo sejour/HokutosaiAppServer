415c8231980dfe660a56dabf70090ad4683775c7
sejour <shuka.takakuma@gmail.com>
2016-03-25 02:49:10 +0900

    [add] ShopRepository
    Shop entityのためのRepositoryを追加

---
+++ src/main/java/hokutosai/server/data/repository/ShopRepository.java
@@ -0,0 +1,9 @@
+package hokutosai.server.data.repository;
+
+import hokutosai.server.data.entity.Shop;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShopRepository extends JpaRepository<Shop, Integer>{
+
+}
