e945e04b41a888a9cb8ebb266323e07fd3f517f4
sejour <shuka.takakuma@gmail.com>
2016-03-25 05:36:52 +0900

    [delete] Old files
    commit 20573140b8c42960bc1f561d1b254e97929371d4 で行ったentityとrepositoryのパッケージ再編に伴い旧ファイルを削除

--- src/main/java/hokutosai/server/data/repository/PlaceRepository.java
+++
@@ -1,9 +0,0 @@
-package hokutosai.server.data.repository;
-
-import hokutosai.server.data.entity.Place;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlaceRepository extends JpaRepository<Place, Integer>  {
-
-}
--- src/main/java/hokutosai/server/data/repository/ShopRepository.java
+++
@@ -1,9 +0,0 @@
-package hokutosai.server.data.repository;
-
-import hokutosai.server.data.entity.Shop;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShopRepository extends JpaRepository<Shop, Integer>{
-
-}
