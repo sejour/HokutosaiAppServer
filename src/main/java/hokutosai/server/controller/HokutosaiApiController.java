92f1fe7417f11b5c71136a553ccf84b7cff2468e
sejour <shuka.takakuma@gmail.com>
2016-03-24 22:12:59 +0900

    [modify] ControllerをRestControllerに変更
    REST APIを実装するため

--- src/main/java/hokutosai/server/controller/HokutosaiApiController.java
+++ src/main/java/hokutosai/server/controller/HokutosaiApiController.java
@@ -1,17 +1,15 @@
 package hokutosai.server.controller;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @EnableAutoConfiguration
 @RequestMapping("/api")
 public class HokutosaiApiController {
 
 	 @RequestMapping("/hello")
-	 @ResponseBody
 	 public String hello() {
 	     return "Hello HokutosaiAPI";
 	 }
