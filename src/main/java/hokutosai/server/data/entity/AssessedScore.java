8ffd09d29634711de4a49e9ecce00aca8437497f
sejour <shuka.takakuma@gmail.com>
2016-03-25 10:10:01 +0900

    [fix] AssessedScoreのshopIdフィールドをShopScoreに移動
    AssessedScoreはShopに依存しない抽象エンティティのため

--- src/main/java/hokutosai/server/data/entity/AssessedScore.java
+++ src/main/java/hokutosai/server/data/entity/AssessedScore.java
@@ -1,10 +1,8 @@
 package hokutosai.server.data.entity;
 
 import javax.persistence.Column;
-import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
@@ -12,10 +10,6 @@ import lombok.Data;
 @MappedSuperclass
 @Data
 public abstract class AssessedScore {
-
-	@Column(name = "shop_id") @Id
-	@JsonIgnore
-	private Integer shopId;
 
 	@Column(name = "total_score")
 	@JsonProperty("total_score")
--- src/main/java/hokutosai/server/data/entity/shops/ShopScore.java
+++ src/main/java/hokutosai/server/data/entity/shops/ShopScore.java
@@ -2,11 +2,20 @@ package hokutosai.server.data.entity.shops;
 
 import hokutosai.server.data.entity.AssessedScore;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "shops_scores")
 public class ShopScore extends AssessedScore {
+
+	@Id
+	@Column(name = "shop_id")
+	@JsonIgnore
+	private Integer shopId;
 
 }
