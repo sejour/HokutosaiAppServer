74741a60012bfee666fc2792f1c1827e2da6f70d
sejour <shuka.takakuma@gmail.com>
2016-03-28 07:44:44 +0900

    [add] FilterConfiguration
    ServletFilterを実装するために追加

---
+++ src/main/java/hokutosai/server/config/FilterConfiguration.java
@@ -0,0 +1,9 @@
+package hokutosai.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class FilterConfiguration extends WebMvcConfigurerAdapter {
+
+}
