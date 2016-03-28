0b28e67ba1ce4e0d8611c3201f9f7a133abb2c4d
sejour <shuka.takakuma@gmail.com>
2016-03-28 16:27:29 +0900

    [add] URI and Automatic generation of messages
    エラー発生のRequestURIを確認するためにURIを追加し，さらにエラーメッセージの自動生成機能を追加.

--- src/main/java/hokutosai/server/error/HokutosaiServerException.java
+++ src/main/java/hokutosai/server/error/HokutosaiServerException.java
@@ -8,25 +8,28 @@ import org.springframework.http.HttpStatus;
 public class HokutosaiServerException extends Exception {
 
 	@Getter
+	protected String uri;
+
+	@Getter
 	protected HttpStatus httpStatus;
 
-	public HokutosaiServerException(HttpStatus httpStatus) {
-		super(httpStatus.toString());
+	public HokutosaiServerException(String uri, HttpStatus httpStatus) {
+		super(String.format("[%s] (%s)", uri, httpStatus.toString()));
 		this.httpStatus = httpStatus;
 	}
 
-	public HokutosaiServerException(HttpStatus httpStatus, Throwable nested) {
-		super(httpStatus.toString(), nested);
+	public HokutosaiServerException(String uri, HttpStatus httpStatus, Throwable nested) {
+		super(String.format("[%s] (%s)", uri, httpStatus.toString()), nested);
 		this.httpStatus = httpStatus;
 	}
 
-	public HokutosaiServerException(HttpStatus httpStatus, String message) {
-		super(message);
+	public HokutosaiServerException(String uri, HttpStatus httpStatus, String message) {
+		super(String.format("[%s] (%s) %s", uri, httpStatus.toString(), message));
 		this.httpStatus = httpStatus;
 	}
 
-	public HokutosaiServerException(HttpStatus httpStatus, String message, Throwable nested) {
-		super(message, nested);
+	public HokutosaiServerException(String uri, HttpStatus httpStatus, String message, Throwable nested) {
+		super(String.format("[%s] (%s) %s", uri, httpStatus.toString(), message), nested);
 		this.httpStatus = httpStatus;
 	}
 
