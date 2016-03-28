ce8b5888ef6573b5ff62e99b4f66d68ce6470495
sejour <shuka.takakuma@gmail.com>
2016-03-28 16:43:26 +0900

    [add] HttpMethod

--- src/main/java/hokutosai/server/error/HokutosaiServerException.java
+++ src/main/java/hokutosai/server/error/HokutosaiServerException.java
@@ -8,28 +8,39 @@ import org.springframework.http.HttpStatus;
 public class HokutosaiServerException extends Exception {
 
 	@Getter
+	protected String httpMethod;
+
+	@Getter
 	protected String uri;
 
 	@Getter
 	protected HttpStatus httpStatus;
 
-	public HokutosaiServerException(String uri, HttpStatus httpStatus) {
-		super(String.format("[%s] (%s)", uri, httpStatus.toString()));
+	public HokutosaiServerException(String httpMethod, String uri, HttpStatus httpStatus) {
+		super(String.format("[%s %s] (%s)", httpMethod, uri, httpStatus.toString()));
+		this.httpMethod = httpMethod;
+		this.uri = uri;
 		this.httpStatus = httpStatus;
 	}
 
-	public HokutosaiServerException(String uri, HttpStatus httpStatus, Throwable nested) {
-		super(String.format("[%s] (%s)", uri, httpStatus.toString()), nested);
+	public HokutosaiServerException(String httpMethod, String uri, HttpStatus httpStatus, Throwable nested) {
+		super(String.format("[%s %s] (%s)", httpMethod, uri, httpStatus.toString()), nested);
+		this.httpMethod = httpMethod;
+		this.uri = uri;
 		this.httpStatus = httpStatus;
 	}
 
-	public HokutosaiServerException(String uri, HttpStatus httpStatus, String message) {
-		super(String.format("[%s] (%s) %s", uri, httpStatus.toString(), message));
+	public HokutosaiServerException(String httpMethod, String uri, HttpStatus httpStatus, String message) {
+		super(String.format("[%s %s] (%s) %s", httpMethod, uri, httpStatus.toString(), message));
+		this.httpMethod = httpMethod;
+		this.uri = uri;
 		this.httpStatus = httpStatus;
 	}
 
-	public HokutosaiServerException(String uri, HttpStatus httpStatus, String message, Throwable nested) {
-		super(String.format("[%s] (%s) %s", uri, httpStatus.toString(), message), nested);
+	public HokutosaiServerException(String httpMethod, String uri, HttpStatus httpStatus, String message, Throwable nested) {
+		super(String.format("[%s %s] (%s) %s", httpMethod, uri, httpStatus.toString(), message), nested);
+		this.httpMethod = httpMethod;
+		this.uri = uri;
 		this.httpStatus = httpStatus;
 	}
 
