187ecb1b8585fdbc7d2327853fbaabcec8268d3d
sejour <shuka.takakuma@gmail.com>
2016-03-28 14:47:16 +0900

    [add] HttpStatus
    例外スロー時にクライアントへ返すHTTPステータスコードを指定するために追加

--- src/main/java/hokutosai/server/error/HokutosaiServerException.java
+++ src/main/java/hokutosai/server/error/HokutosaiServerException.java
@@ -1,14 +1,33 @@
 package hokutosai.server.error;
 
+import lombok.Getter;
+
+import org.springframework.http.HttpStatus;
+
 @SuppressWarnings("serial")
 public class HokutosaiServerException extends Exception {
 
-	public HokutosaiServerException(String message) {
-		super(message);
+	@Getter
+	protected HttpStatus httpStatus;
+
+	public HokutosaiServerException(HttpStatus httpStatus) {
+		super(httpStatus.toString());
+		this.httpStatus = httpStatus;
 	}
 
-	public HokutosaiServerException(String message, Throwable nested) {
+	public HokutosaiServerException(HttpStatus httpStatus, Throwable nested) {
+		super(httpStatus.toString(), nested);
+		this.httpStatus = httpStatus;
+	}
+
+	public HokutosaiServerException(HttpStatus httpStatus, String message) {
+		super(message);
+		this.httpStatus = httpStatus;
+	}
+
+	public HokutosaiServerException(HttpStatus httpStatus, String message, Throwable nested) {
 		super(message, nested);
+		this.httpStatus = httpStatus;
 	}
 
 }
