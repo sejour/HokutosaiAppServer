e971efac6a6d602d960246c09c97ff4c3f1e4eaa
sejour <shuka.takakuma@gmail.com>
2016-03-29 21:17:53 +0900

    [add] ロールが分からないときはunknownになるように実装

--- src/main/java/hokutosai/server/data/domain/AuthorizationApiUser.java
+++ src/main/java/hokutosai/server/data/domain/AuthorizationApiUser.java
@@ -9,7 +9,12 @@ public class AuthorizationApiUser {
 
 	private String userId;
 
-	public AuthorizationApiUser(String role, String userId) {
+	public AuthorizationApiUser(String userId) {
+		this.role = "<<unknown>>";
+		this.userId = userId;
+	}
+
+	public AuthorizationApiUser(String userId, String role) {
 		this.role = role;
 		this.userId = userId;
 	}
