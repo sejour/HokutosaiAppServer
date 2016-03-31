a706d77d6acd75f7d2e2f9883c8167928c0a0f6a
sejour <shuka.takakuma@gmail.com>
2016-03-31 13:41:24 +0900

    [modify] 500エラーのときはMongoDBに加えログファイルにもエラー情報を出力するように変更
    Internal Server Errorの監視を強化するため

--- src/main/java/hokutosai/server/log/AccessLogger.java
+++ src/main/java/hokutosai/server/log/AccessLogger.java
@@ -5,6 +5,8 @@ import hokutosai.server.data.domain.log.AccessLog;
 import hokutosai.server.data.repository.log.AccessErrorLogRepository;
 import hokutosai.server.data.repository.log.AccessLogRepository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +19,18 @@ public class AccessLogger {
 	@Autowired
 	private AccessErrorLogRepository accessErrorLogRepository;
 
+	private static final Logger logger = LoggerFactory.getLogger(AccessLogger.class);
+
 	public void access(AccessLog accessLog) {
 		this.accessLogRepository.save(accessLog);
 	}
 
 	public void error(AccessErrorLog errorLog) {
 		this.accessErrorLogRepository.save(errorLog);
+
+		if (errorLog.getStatus().getCode() == 500) {
+			logger.error(errorLog.getException().getMessage());
+		}
 	}
 
 }
