c0b03acbb0dad06f20a47683441add5315321cd9
sejour <shuka.takakuma@gmail.com>
2016-03-31 19:30:24 +0900

    [delete] Objectを引数にとるコンストラクタを削除

--- src/main/java/hokutosai/server/error/InvalidParameterValueException.java
+++ src/main/java/hokutosai/server/error/InvalidParameterValueException.java
@@ -1,12 +1,7 @@
 package hokutosai.server.error;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @SuppressWarnings("serial")
 public class InvalidParameterValueException extends BadRequestException {
-
-	public InvalidParameterValueException(Object parameter) {
-		this(parameter.getClass().getAnnotation(RequestParam.class).value(), parameter.toString());
-	}
 
 	public InvalidParameterValueException(String parameterName, Object parameterValue) {
 		this(parameterName, parameterValue.toString());
