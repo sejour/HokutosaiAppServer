a3490774c603ea4ebcac9f86db26345a06e2807c
sejour <shuka.takakuma@gmail.com>
2016-04-02 14:07:00 +0900

    [modify] DBフィールド名の指定

--- src/main/java/hokutosai/server/data/repository/auth/EndpointApiUserPermissionRepository.java
+++ src/main/java/hokutosai/server/data/repository/auth/EndpointApiUserPermissionRepository.java
@@ -3,9 +3,10 @@ package hokutosai.server.data.repository.auth;
 import hokutosai.server.data.entity.auth.EndpointApiUserPermission;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface EndpointApiUserPermissionRepository extends JpaRepository<EndpointApiUserPermission, Integer> {
 
-	public EndpointApiUserPermission findByPathAndMethodAndRole(String path, String method, String role);
+	public EndpointApiUserPermission findByPathAndMethodAndRole(String path, String method, @Param("api_user_role") String role);
 
 }
