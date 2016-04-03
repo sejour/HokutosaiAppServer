06ccf68b10751efc12e3aac237f3a9c8a23a5b38
sejour <shuka.takakuma@gmail.com>
2016-04-03 20:41:41 +0900

    [modify] 認証アカウントをServletRequestのAttributeに設定
    アカウント情報をロギングするため

--- src/main/java/hokutosai/server/controller/AccountsController.java
+++ src/main/java/hokutosai/server/controller/AccountsController.java
@@ -1,10 +1,13 @@
 package hokutosai.server.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import hokutosai.server.data.json.account.AuthorizedAccount;
 import hokutosai.server.error.InternalServerErrorException;
 import hokutosai.server.security.auth.AccountAuthorizer;
 import hokutosai.server.security.auth.AccountForbiddenException;
 import hokutosai.server.security.auth.AccountUnauthorizedException;
+import hokutosai.server.util.RequestAttribute;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -22,8 +25,10 @@ public class AccountsController {
 	AccountAuthorizer accountAuthorizer;
 
 	@RequestMapping(value = "/auth", method = RequestMethod.POST)
-	public AuthorizedAccount postAuth(@RequestParam("account_id") String id, @RequestParam("account_pass") String password) throws AccountUnauthorizedException, AccountForbiddenException, InternalServerErrorException {
-		return this.accountAuthorizer.loginAuthorize(id, password);
+	public AuthorizedAccount postAuth(HttpServletRequest request, @RequestParam("account_id") String id, @RequestParam("account_pass") String password) throws AccountUnauthorizedException, AccountForbiddenException, InternalServerErrorException {
+		AuthorizedAccount account = this.accountAuthorizer.loginAuthorize(id, password);
+		request.setAttribute(RequestAttribute.ACCOUNT, account);
+		return account;
 	}
 
 }
