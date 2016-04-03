6589bae654ef21e7e1837baea70122bcde7a2474
sejour <shuka.takakuma@gmail.com>
2016-04-03 18:27:57 +0900

    [change] エラーハンドリングとアクセスロギング/エラーロギングの方式を改変
    エラー処理とロギング処理の一元化のため

--- src/main/java/hokutosai/server/filter/HokutosaiApiFilter.java
+++ src/main/java/hokutosai/server/filter/HokutosaiApiFilter.java
@@ -1,24 +1,21 @@
 package hokutosai.server.filter;
 
 import hokutosai.server.data.document.auth.AuthorizationTarget;
-import hokutosai.server.data.document.log.AccessErrorLog;
-import hokutosai.server.data.document.log.AccessLog;
 import hokutosai.server.data.entity.Endpoint;
 import hokutosai.server.data.json.account.AuthorizedAccount;
 import hokutosai.server.data.repository.EndpointRepository;
+import hokutosai.server.error.ErrorHandler;
 import hokutosai.server.error.ForbiddenException;
-import hokutosai.server.error.HokutosaiServerException;
 import hokutosai.server.error.NotFoundException;
 import hokutosai.server.error.UnauthorizedException;
 import hokutosai.server.error.response.ErrorResponse;
 import hokutosai.server.log.AccessLogger;
 import hokutosai.server.security.auth.AccountAuthorizer;
 import hokutosai.server.security.auth.ApiUserAuthorizer;
-import hokutosai.server.security.auth.ApiUserForbiddenException;
-import hokutosai.server.security.auth.ApiUserUnauthorizedException;
 import hokutosai.server.security.auth.AuthorizationHeader;
 import hokutosai.server.security.auth.Authorizer;
 import hokutosai.server.util.EndpointPath;
+import hokutosai.server.util.RequestAttribute;
 
 import java.io.IOException;
 
@@ -34,7 +31,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -64,35 +60,31 @@ public class HokutosaiApiFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest httpRequest = (HttpServletRequest)request;
-		AuthorizationTarget apiUser = null;
+		HttpServletResponse httpResponse = (HttpServletResponse)response;
 
 		try {
 			Endpoint endpoint = this.acceptRequest(httpRequest);
 			AuthorizationHeader authHeader = new AuthorizationHeader(httpRequest);
-			apiUser = this.apiUserAuthorizer.authorize(authHeader.getApiUser(), endpoint);
-			request.setAttribute("ApiUser", apiUser);
+			AuthorizationTarget apiUser = this.apiUserAuthorizer.authorize(authHeader.getApiUser(), endpoint);
+			request.setAttribute(RequestAttribute.API_USER, apiUser);
 
 			String accountNeed = endpoint.getAccountNeed();
 			if (!accountNeed.equals("no")) {
 				if (authHeader.hasAccount()) {
 					AuthorizedAccount account = this.accountAuthorizer.authorize(authHeader.getAccount(), endpoint);
-					request.setAttribute("Account", account);
+					request.setAttribute(RequestAttribute.ACCOUNT, account);
 				} else if (accountNeed.equals("required")) {
 					throw new UnauthorizedException("Account is required to access to the endpoint.");
 				}
 			}
 
 			chain.doFilter(request, response);
-			this.accessLogger.access(new AccessLog(httpRequest, apiUser, HttpStatus.valueOf(((HttpServletResponse)response).getStatus())));
-		}
-		catch (ApiUserUnauthorizedException e) {
-			this.handleException(e, httpRequest, response, e.getApiUser());
-		}
-		catch (ApiUserForbiddenException e) {
-			this.handleException(e, httpRequest, response, e.getApiUser());
 		}
 		catch (Throwable e) {
-			this.handleException(e, httpRequest, response, apiUser);
+			this.respondError(ErrorHandler.handle(e, request), httpResponse);
+		}
+		finally {
+			this.accessLogger.log(httpRequest, httpResponse);
 		}
 	}
 
@@ -108,27 +100,10 @@ public class HokutosaiApiFilter implements Filter {
 		throw new ForbiddenException("Access is not allowed.");
 	}
 
-	private void handleException(Throwable threw, HttpServletRequest httpRequest, ServletResponse response, AuthorizationTarget apiUser) {
-		if (threw == null) { return; }
-
-		HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
-
-		for (Throwable e = threw; e != null; e = e.getCause()) {
-			if (e instanceof HokutosaiServerException) {
-				HokutosaiServerException hse = (HokutosaiServerException)e;
-				status = hse.getHttpStatus();
-				threw = e;
-				break;
-			}
-		}
-
-		this.accessLogger.error(new AccessErrorLog(httpRequest, apiUser, status, threw));
-		this.respondError(status, status.is5xxServerError() ? null : threw.getMessage(), response);
-	}
-
-	private void respondError(HttpStatus status, String message, ServletResponse response) {
+	private void respondError(ErrorResponse error, HttpServletResponse response) {
 		try {
-			this.om.writeValue(response.getOutputStream(), new ErrorResponse(status, message));
+			response.setStatus(error.getHttpStatus().value());
+			this.om.writeValue(response.getOutputStream(), error);
 		} catch (IOException e) {
 			logger.error(e.toString());
 		}
