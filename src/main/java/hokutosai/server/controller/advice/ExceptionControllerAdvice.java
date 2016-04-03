e8b830ebbe073f7883b0fd001441e92a33444671
sejour <shuka.takakuma@gmail.com>
2016-04-03 17:24:23 +0900

    [implement] 各Controllerで発生した例外のハンドリングを実装

--- src/main/java/hokutosai/server/controller/advice/ExceptionControllerAdvice.java
+++ src/main/java/hokutosai/server/controller/advice/ExceptionControllerAdvice.java
@@ -1,16 +1,25 @@
 package hokutosai.server.controller.advice;
 
-import org.springframework.http.HttpStatus;
+import hokutosai.server.error.response.ErrorResponse;
+import hokutosai.server.security.auth.AuthorizationAccount;
+import hokutosai.server.error.ErrorHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 public class ExceptionControllerAdvice {
 
 	@ExceptionHandler(Throwable.class)
-    public ResponseEntity<String> exception(Throwable e) throws Throwable {
-		return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+	@ResponseBody
+    public ResponseEntity<ErrorResponse> exception(HttpServletRequest request, Throwable e) {
+		if (e instanceof AuthorizationAccount) request.setAttribute("Account", ((AuthorizationAccount)e).getAccount());
+		ErrorResponse error = ErrorHandler.handle(e, request);
+		return new ResponseEntity<>(error, error.getHttpStatus());
     }
 
 }
