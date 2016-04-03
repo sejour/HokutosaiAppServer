6c8e48bf0fee6c84288e1f23d7984dec6db61b8d
sejour <shuka.takakuma@gmail.com>
2016-04-03 16:44:04 +0900

    [change] HttpServletRequestとHttpServletResponseかログドキュメントを作成しロギングするように変更
    アクセスロギングをHokutosaiApiFilter内で統一化するため

--- src/main/java/hokutosai/server/log/AccessLogger.java
+++ src/main/java/hokutosai/server/log/AccessLogger.java
@@ -1,16 +1,17 @@
 package hokutosai.server.log;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import hokutosai.server.data.document.auth.AuthorizationTarget;
 import hokutosai.server.data.document.log.AccessErrorLog;
 import hokutosai.server.data.document.log.AccessLog;
 import hokutosai.server.data.repository.log.AccessErrorLogRepository;
 import hokutosai.server.data.repository.log.AccessLogRepository;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.http.HttpStatus;
 
-@Component
 public class AccessLogger {
 
 	@Autowired
@@ -19,17 +20,23 @@ public class AccessLogger {
 	@Autowired
 	private AccessErrorLogRepository accessErrorLogRepository;
 
-	private static final Logger logger = LoggerFactory.getLogger(AccessLogger.class);
+	public void log(HttpServletRequest request, HttpServletResponse response) {
+		AuthorizationTarget apiUser = null;
+		AuthorizationTarget account = null;
+		HttpStatus httpStatus = HttpStatus.valueOf(response.getStatus());
 
-	public void access(AccessLog accessLog) {
-		this.accessLogRepository.save(accessLog);
-	}
+		Object apiUserAttribute = request.getAttribute("apiUser");
+		if (apiUserAttribute != null) apiUser = (AuthorizationTarget)apiUserAttribute;
 
-	public void error(AccessErrorLog errorLog) {
-		this.accessErrorLogRepository.save(errorLog);
+		Object accountAttribute = request.getAttribute("account");
+		if (accountAttribute != null) account = (AuthorizationTarget)accountAttribute;
 
-		if (errorLog.getStatus().getCode() == 500) {
-			logger.error(errorLog.getException().getMessage());
+		Object error = request.getAttribute("Error");
+		if (error == null) {
+			this.accessLogRepository.save(new AccessLog(request, apiUser, account, httpStatus));
+		}
+		else {
+			this.accessErrorLogRepository.save(new AccessErrorLog(request, apiUser, account, httpStatus, (Throwable)error));
 		}
 	}
 
