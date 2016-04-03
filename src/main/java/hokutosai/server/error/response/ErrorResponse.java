bad1d1bdbb0ca765171d1099e4eec3aadefa854c
sejour <shuka.takakuma@gmail.com>
2016-04-03 17:15:43 +0900

    [add] ErrorResponseにHttpStatusフィールドを追加

--- src/main/java/hokutosai/server/error/response/ErrorResponse.java
+++ src/main/java/hokutosai/server/error/response/ErrorResponse.java
@@ -2,6 +2,7 @@ package hokutosai.server.error.response;
 
 import org.springframework.http.HttpStatus;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
@@ -12,8 +13,13 @@ public class ErrorResponse {
 	@JsonProperty("error")
 	private Error error;
 
+	@Getter
+	@JsonIgnore
+	private HttpStatus httpStatus;
+
 	public ErrorResponse(HttpStatus status, String message) {
 		this.error = new Error(status, message);
+		this.httpStatus = status;
 	}
 
 }
