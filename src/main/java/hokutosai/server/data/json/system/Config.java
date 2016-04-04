1fd0137ff681be0ce54b90af021074e8c768db43
sejour <shuka.takakuma@gmail.com>
2016-04-04 23:12:41 +0900

    [add] Hokutosai server config

---
+++ src/main/java/hokutosai/server/data/json/system/Config.java
@@ -0,0 +1,38 @@
+package hokutosai.server.data.json.system;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+
+@Component
+public class Config {
+
+	@Value("${hokutosai.server.name}")
+	@JsonProperty("name")
+	@Getter
+	private String name;
+
+	@Value("${hokutosai.server.target}")
+	@JsonProperty("target")
+	@Getter
+	private Integer targetYear;
+
+	@Value("${hokutosai.server.version}")
+	@JsonProperty("version")
+	@Getter
+	private String version;
+
+	@Value("${spring.jackson.dateFormat}")
+	@JsonProperty("datetime_format")
+	@Getter
+	private String datetimeFormat;
+
+	@Value("${spring.jackson.time-zone}")
+	@JsonProperty("time_zone")
+	@Getter
+	private String timeZone;
+
+}
