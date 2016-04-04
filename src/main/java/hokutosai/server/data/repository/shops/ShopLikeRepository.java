91d709323f6c4da104b048d3ce38713fd587845f
sejour <shuka.takakuma@gmail.com>
2016-04-05 00:42:27 +0900

    [fix] 戻り値をListに修正

--- src/main/java/hokutosai/server/data/repository/shops/ShopLikeRepository.java
+++ src/main/java/hokutosai/server/data/repository/shops/ShopLikeRepository.java
@@ -1,12 +1,14 @@
 package hokutosai.server.data.repository.shops;
 
+import java.util.List;
+
 import hokutosai.server.data.entity.shops.ShopLike;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ShopLikeRepository extends JpaRepository<ShopLike, Integer> {
 
-	public ShopLike findByAccountId(String accountId);
+	public List<ShopLike> findByAccountId(String accountId);
 
 	public ShopLike findByShopIdAndAccountId(Integer shopId, String accountId);
 
