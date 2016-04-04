e7acd75be0dffc9e3cf165a7217a9f6ebb1263c5
sejour <shuka.takakuma@gmail.com>
2016-04-04 12:46:04 +0900

    [add] updateAssess method
    再評価の際にレコードを更新するため

--- src/main/java/hokutosai/server/data/repository/shops/ShopAssessRepository.java
+++ src/main/java/hokutosai/server/data/repository/shops/ShopAssessRepository.java
@@ -1,11 +1,21 @@
 package hokutosai.server.data.repository.shops;
 
+import javax.transaction.Transactional;
+
 import hokutosai.server.data.entity.shops.ShopAssess;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ShopAssessRepository extends JpaRepository<ShopAssess, Integer> {
 
 	public ShopAssess findByShopIdAndAccountId(Integer shopId, String accountId);
+
+	@Modifying
+	@Transactional
+	@Query("UPDATE ShopAssess a SET a.score = :score WHERE a.accountId = :accountId AND a.shopId = :shopId")
+	public void updateAssess(@Param("accountId") Integer accountId, @Param("shopId") Integer shopId, @Param("score") Integer score);
 
 }
