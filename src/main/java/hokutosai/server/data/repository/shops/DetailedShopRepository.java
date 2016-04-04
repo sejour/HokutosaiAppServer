05b31952e3e29552e479e9c995c07c3593dba27d
sejour <shuka.takakuma@gmail.com>
2016-04-05 02:49:04 +0900

    [add] LikesCountをインクリメント/デクリメントするクエリを実装

--- src/main/java/hokutosai/server/data/repository/shops/DetailedShopRepository.java
+++ src/main/java/hokutosai/server/data/repository/shops/DetailedShopRepository.java
@@ -1,11 +1,26 @@
 package hokutosai.server.data.repository.shops;
 
+import javax.transaction.Transactional;
+
 import hokutosai.server.data.entity.shops.DetailedShop;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface DetailedShopRepository extends JpaRepository<DetailedShop, Integer> {
 
 	public DetailedShop findByShopId(Integer shopId);
+
+	@Modifying
+	@Transactional
+	@Query("UPDATE DetailedShop s SET s.likesCount = s.likesCount + 1 WHERE s.shopId = :shopId")
+	public void incrementLikesCount(@Param("shopId") Integer shopId);
+
+	@Modifying
+	@Transactional
+	@Query("UPDATE DetailedShop s SET s.likesCount = s.likesCount - 1 WHERE s.shopId = :shopId")
+	public void decrementLikesCount(@Param("shopId") Integer shopId);
 
 }
