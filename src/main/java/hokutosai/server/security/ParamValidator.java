762880db5519ffb93c0e2815f1003c0a0b38da28
sejour <shuka.takakuma@gmail.com>
2016-04-04 17:00:19 +0900

    [add] テキストのValidatorを追加
    空文字，空白のみ，最大長を超過するテキストはエラーとするため

--- src/main/java/hokutosai/server/security/ParamValidator.java
+++ src/main/java/hokutosai/server/security/ParamValidator.java
@@ -1,11 +1,21 @@
 package hokutosai.server.security;
 
+import org.springframework.util.StringUtils;
+
 import hokutosai.server.error.InvalidParameterValueException;
 
 public class ParamValidator {
 
 	public static void range(String parameterName, Integer value, int min, int max) throws InvalidParameterValueException {
 		if (value == null || value < min || value > max) throw new InvalidParameterValueException(parameterName, value);
+	}
+
+	public static void text(String parameterName, String value) throws InvalidParameterValueException {
+		if (!StringUtils.hasText(value)) throw new InvalidParameterValueException(parameterName, value);
+	}
+
+	public static void text(String parameterName, String value, Integer maxLength) throws InvalidParameterValueException {
+		if (!StringUtils.hasText(value) || value.length() > maxLength) throw new InvalidParameterValueException(parameterName, value);
 	}
 
 }
