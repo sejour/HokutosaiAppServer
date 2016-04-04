9d03e8b9d826ef3314cdfe47224f97192c834b5b
sejour <shuka.takakuma@gmail.com>
2016-04-05 00:09:10 +0900

    [add] ShopLikeRepository

---
+++ src/main/java/hokutosai/server/data/repository/shops/ShopLikeRepository.java
@@ -0,0 +1,11 @@
+package hokutosai.server.data.repository.shops;
+
+import hokutosai.server.data.entity.shops.ShopLike;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShopLikeRepository extends JpaRepository<ShopLike, Integer> {
+
+	public ShopLike findByShopIdAndAccountId(String shopId, String accountId);
+
+}
