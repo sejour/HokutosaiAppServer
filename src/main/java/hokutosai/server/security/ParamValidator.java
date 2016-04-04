7b7363d1c2df5c87a20f1a909761ec67ef4b66f8
sejour <shuka.takakuma@gmail.com>
2016-04-04 22:02:46 +0900

    [modify] Validatonが正常に完了すれば対象の値を返すように変更

--- src/main/java/hokutosai/server/security/ParamValidator.java
+++ src/main/java/hokutosai/server/security/ParamValidator.java
@@ -6,16 +6,19 @@ import hokutosai.server.error.InvalidParameterValueException;
 
 public class ParamValidator {
 
-	public static void range(String parameterName, Integer value, int min, int max) throws InvalidParameterValueException {
+	public static Integer range(String parameterName, Integer value, int min, int max) throws InvalidParameterValueException {
 		if (value == null || value < min || value > max) throw new InvalidParameterValueException(parameterName, value);
+		return value;
 	}
 
-	public static void text(String parameterName, String value) throws InvalidParameterValueException {
+	public static String text(String parameterName, String value) throws InvalidParameterValueException {
 		if (!StringUtils.hasText(value)) throw new InvalidParameterValueException(parameterName, value);
+		return value;
 	}
 
-	public static void text(String parameterName, String value, Integer maxLength) throws InvalidParameterValueException {
+	public static String text(String parameterName, String value, Integer maxLength) throws InvalidParameterValueException {
 		if (!StringUtils.hasText(value) || value.length() > maxLength) throw new InvalidParameterValueException(parameterName, value);
+		return value;
 	}
 
 }
