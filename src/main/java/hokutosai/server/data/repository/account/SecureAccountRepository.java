96ffffc04eb28e8f58a314da056f85d8c0c8fe4c
sejour <shuka.takakuma@gmail.com>
2016-04-05 11:50:21 +0900

    [add] SecureAccountRepository

---
+++ src/main/java/hokutosai/server/data/repository/account/SecureAccountRepository.java
@@ -0,0 +1,19 @@
+package hokutosai.server.data.repository.account;
+
+import javax.transaction.Transactional;
+
+import hokutosai.server.data.entity.account.SecureAccount;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface SecureAccountRepository extends JpaRepository<SecureAccount, String> {
+
+	@Modifying
+	@Transactional
+	@Query("UPDATE SecureAccount a SET a.name = :name WHERE a.accountId = :accountId")
+	public void updateName(@Param("accountId") Integer accountId, @Param("name") String userName);
+
+}
