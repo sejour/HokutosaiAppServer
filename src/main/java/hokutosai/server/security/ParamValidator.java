459b962df90ea289ed3f3867499d296929dccc82
sejour <shuka.takakuma@gmail.com>
2016-04-11 00:00:07 +0900

    [add] Validation結果のErrorsを検査するメソッドを追加

--- src/main/java/hokutosai/server/security/ParamValidator.java
+++ src/main/java/hokutosai/server/security/ParamValidator.java
@@ -1,7 +1,10 @@
 package hokutosai.server.security;
 
 import org.springframework.util.StringUtils;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
 
+import hokutosai.server.error.BadRequestException;
 import hokutosai.server.error.InvalidParameterValueException;
 
 public class ParamValidator {
@@ -19,6 +22,17 @@ public class ParamValidator {
 	public static String text(String parameterName, String value, Integer maxLength) throws InvalidParameterValueException {
 		if (!StringUtils.hasText(value) || value.length() > maxLength) throw new InvalidParameterValueException(parameterName, value);
 		return value;
+	}
+
+	public static void checkErrors(Errors errors) throws BadRequestException {
+		if (errors.hasErrors()) {
+			StringBuilder message = new StringBuilder("[");
+			for (ObjectError error : errors.getAllErrors()) {
+				message.append(String.format("%s\"%s\"", message.equals("[") ? "" : ",", error.getDefaultMessage()));
+			}
+			message.append("]");
+			throw new BadRequestException(message.toString());
+		}
 	}
 
 }
