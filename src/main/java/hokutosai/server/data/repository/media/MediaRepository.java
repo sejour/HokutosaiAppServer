b64e72c8309b19f6d736e516b7933603f3f2a939
sejour <shuka.takakuma@gmail.com>
2016-04-10 18:08:28 +0900

    [add] Media repository

---
+++ src/main/java/hokutosai/server/data/repository/media/MediaRepository.java
@@ -0,0 +1,9 @@
+package hokutosai.server.data.repository.media;
+
+import hokutosai.server.data.entity.media.Media;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MediaRepository extends JpaRepository<Media, Integer> {
+
+}
