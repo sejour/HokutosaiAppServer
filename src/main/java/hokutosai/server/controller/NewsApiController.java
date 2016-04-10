a9998ba823eb10e74c2d3b132eab350dd10a84f8
sejour <shuka.takakuma@gmail.com>
2016-04-11 00:20:15 +0900

    [add] POST /news/article
    ニュースを投稿するためのAPIを実装

--- src/main/java/hokutosai/server/controller/NewsApiController.java
+++ src/main/java/hokutosai/server/controller/NewsApiController.java
@@ -1,7 +1,22 @@
 package hokutosai.server.controller;
 
+import java.util.Date;
+
+import javax.validation.Valid;
+
+import hokutosai.server.config.MediaConfiguration;
+import hokutosai.server.data.entity.news.News;
+import hokutosai.server.data.repository.news.NewsRepository;
+import hokutosai.server.error.BadRequestException;
+import hokutosai.server.security.ParamValidator;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +24,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/news")
 public class NewsApiController {
 
+	@Autowired
+	private MediaConfiguration mediaConfig;
 
+	@Autowired
+	private NewsRepository newsRepository;
+
+	@RequestMapping(value = "/article", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE + ";charset=utf-8")
+	public News postArticle(@RequestBody @Valid News news, Errors errors) throws BadRequestException {
+		ParamValidator.checkErrors(errors);
+		news.setDatetime(new Date());
+		return this.newsRepository.save(news);
+	}
 
 }
