11469d9daa8076c1f80b85fcb21a986b665f2c08
sejour <shuka.takakuma@gmail.com>
2016-04-11 04:07:29 +0900

    [add] findByNewsIdOrderBySequenceAscメソッドを追加
    特定のニュースの添付画像をシーケンス順に取り出すため

--- src/main/java/hokutosai/server/data/repository/news/NewsWithMediaRepository.java
+++ src/main/java/hokutosai/server/data/repository/news/NewsWithMediaRepository.java
@@ -1,9 +1,13 @@
 package hokutosai.server.data.repository.news;
 
+import java.util.List;
+
 import hokutosai.server.data.entity.news.NewsWithMedia;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NewsWithMediaRepository extends JpaRepository<NewsWithMedia, Integer> {
+
+	public List<NewsWithMedia> findByNewsIdOrderBySequenceAsc(Integer newsId);
 
 }
