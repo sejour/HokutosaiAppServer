598786ff0648c2953a376aeba25433589f3bbba0
sejour <shuka.takakuma@gmail.com>
2016-04-10 20:14:28 +0900

    [add] registUrlメソッドをUPDATEクエリで定義
    Mediaの書き込み完了後にDBにURLを登録するため

--- src/main/java/hokutosai/server/data/repository/media/MediaRepository.java
+++ src/main/java/hokutosai/server/data/repository/media/MediaRepository.java
@@ -1,9 +1,19 @@
 package hokutosai.server.data.repository.media;
 
+import javax.transaction.Transactional;
+
 import hokutosai.server.data.entity.media.Media;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MediaRepository extends JpaRepository<Media, Integer> {
+
+	@Modifying
+	@Transactional
+	@Query("UPDATE Media m SET m.url = :url WHERE m.mediaId = :mediaId")
+	public void registUrl(@Param("mediaId") Integer mediaId, @Param("url") String url);
 
 }
