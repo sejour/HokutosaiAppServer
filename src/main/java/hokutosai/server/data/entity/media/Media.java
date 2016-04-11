f54563018d762fca8ffa9b84b41d477e608991c7
sejour <shuka.takakuma@gmail.com>
2016-04-12 06:52:10 +0900

    [modify] SuperMediaを継承するように変更

--- src/main/java/hokutosai/server/data/entity/media/Media.java
+++ src/main/java/hokutosai/server/data/entity/media/Media.java
@@ -1,44 +1,16 @@
 package hokutosai.server.data.entity.media;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import lombok.Data;
 
 @Entity
 @Table(name = "medias")
-@Data
-public class Media {
+public class Media extends SuperMedia {
 
-	@Id
-	@Column(name = "media_id") @GeneratedValue(strategy = GenerationType.AUTO)
-	@JsonProperty("media_id")
-	@NotNull
-	private Integer mediaId;
-
-	@Column(name = "url")
-	@JsonProperty("url")
-	private String url;
-
-	@Column(name = "file_name")
-	@JsonProperty("name")
-	private String fileName;
-
-	@Column(name = "type", nullable = false)
-	@JsonProperty("type")
-	private String type;
-
-	public Media() { }
+	public Media() {}
 
 	public Media(String type) {
-		this.type = type;
+		super(type);
 	}
 
 }
