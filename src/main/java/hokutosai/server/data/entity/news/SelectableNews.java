c087bd601c926986aa80904e141ae356d1179b45
sejour <shuka.takakuma@gmail.com>
2016-04-15 00:53:22 +0900

    [add] OrderBy annotation
    シーケンス番号で昇順にそーとするため

--- src/main/java/hokutosai/server/data/entity/news/SelectableNews.java
+++ src/main/java/hokutosai/server/data/entity/news/SelectableNews.java
@@ -8,6 +8,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -22,6 +23,7 @@ public class SelectableNews extends News {
 
 	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
 	@JoinColumn(name = "shop_id", insertable = false, updatable = false)
+	@OrderBy("sequence ASC")
 	@JsonProperty("medias")
 	@Getter @Setter
 	private List<MediaWithNews> medias;
