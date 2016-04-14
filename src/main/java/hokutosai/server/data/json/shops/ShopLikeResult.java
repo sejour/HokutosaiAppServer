d18efe047d2e011b5fe02e5164f031822d5931c7
Shuka Takakuma <shuka.takakuma@gmail.com>
2016-04-14 18:30:42 +0900

    [modify] ShopLikeResultをentityからjsonオブジェクトに変更

--- src/main/java/hokutosai/server/data/json/shops/ShopLikeResult.java
+++ src/main/java/hokutosai/server/data/json/shops/ShopLikeResult.java
@@ -1,31 +1,19 @@
 package hokutosai.server.data.json.shops;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Transient;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import hokutosai.server.data.entity.shops.Shop;
+import lombok.Data;
 
-@Entity
-@Table(name = "shops")
+@Data
 public class ShopLikeResult {
 
-	@Id
-	@Column(name = "shop_id") @GeneratedValue(strategy = GenerationType.AUTO)
 	@JsonProperty("shop_id")
 	private Integer shopId;
 	
-	@Transient
 	@JsonProperty("liked")
 	private boolean liked;
 
-	@Column(name = "likes_count")
 	@JsonProperty("likes_count")
 	private  Integer likesCount;
 	
