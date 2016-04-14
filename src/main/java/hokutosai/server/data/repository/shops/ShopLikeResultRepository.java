ae2870f33170c9b75ccf6539ba053d97711dfd03
Shuka Takakuma <shuka.takakuma@gmail.com>
2016-04-14 18:20:02 +0900

    [delete] ShopLikeResultRepository

--- src/main/java/hokutosai/server/data/repository/shops/ShopLikeResultRepository.java
+++
@@ -1,9 +0,0 @@
-package hokutosai.server.data.repository.shops;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import hokutosai.server.data.entity.shops.ShopLikeResult;
-
-public interface ShopLikeResultRepository extends JpaRepository<ShopLikeResult, Integer> {
-
-}
