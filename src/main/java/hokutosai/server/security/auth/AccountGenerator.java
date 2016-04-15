09c9f7ca41b8f911aadd660241b57a4e18c929e8
Shuka Takakuma <shuka.takakuma@gmail.com>
2016-04-15 18:37:31 +0900

    [add] AccountGeneratorクラス
    エンドユーザ向けの新規アカウントを発行するため

---
+++ src/main/java/hokutosai/server/security/auth/AccountGenerator.java
@@ -0,0 +1,47 @@
+package hokutosai.server.security.auth;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import hokutosai.server.data.entity.account.AccountMaster;
+import hokutosai.server.data.entity.account.AccountRole;
+import hokutosai.server.data.repository.account.AccountMasterRepository;
+import hokutosai.server.error.InternalServerErrorException;
+import hokutosai.server.util.RandomToken;
+
+@Component
+public class AccountGenerator {
+
+	@Autowired
+	private AccountMasterRepository accountRepository;
+	
+	private static final int AUTO_LOGIN_PASS_LENGTH = 64;
+	
+	private static final String AUTO_LOGIN_ACCOUNT_ROLE = "general";
+	
+	private RandomToken randToken = new RandomToken();
+	
+	private static final int MAX_RETRY_COUNT = 100;
+	
+	public AccountMaster issue() throws InternalServerErrorException {
+		int count = 1;
+		String id = UUID.randomUUID().toString();
+		while (this.accountRepository.exists(id)) {
+			if (++count >= MAX_RETRY_COUNT) throw new InternalServerErrorException("Sorry, account failed to create.");
+			id = UUID.randomUUID().toString();
+		}
+		
+		String pass = randToken.generate(AUTO_LOGIN_PASS_LENGTH);
+		
+		AccountRole role = new AccountRole(AUTO_LOGIN_ACCOUNT_ROLE, "allow");
+		AccountMaster account = new AccountMaster(id, pass, role);
+		
+		this.accountRepository.save(account);
+		
+		return account;
+	}
+ 	
+}
