85bbb316b80dda5036f425586642af88d2de48f5
sejour <shuka.takakuma@gmail.com>
2016-04-16 12:52:43 +0900

    [add] JsonIgnoreアノテーションをpermissionフィールドに追加
    レスポンスボディにパーミッションは含めないため

--- src/main/java/hokutosai/server/data/entity/auth/Permission.java
+++ src/main/java/hokutosai/server/data/entity/auth/Permission.java
@@ -3,6 +3,8 @@ package hokutosai.server.data.entity.auth;
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,11 +12,12 @@ import lombok.Setter;
 public abstract class Permission {
 
 	@Column(name = "permission")
+	@JsonIgnore
 	@Getter @Setter
 	private String permission;
-	
+
 	public Permission() { }
-	
+
 	public Permission(String permission) {
 		this.permission = permission;
 	}
