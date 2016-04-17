3e2630a823a8b783e50cc58ebf136d957dd61109
sejour <shuka.takakuma@gmail.com>
2016-04-17 19:30:07 +0900

    [add] extends JpaSpecificationExecutor<SelectableNews>
    Specificationを使用して動的クエリを生成するため

--- src/main/java/hokutosai/server/data/repository/news/SelectableNewsRepository.java
+++ src/main/java/hokutosai/server/data/repository/news/SelectableNewsRepository.java
@@ -3,7 +3,8 @@ package hokutosai.server.data.repository.news;
 import hokutosai.server.data.entity.news.SelectableNews;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface SelectableNewsRepository extends JpaRepository<SelectableNews, Integer> {
+public interface SelectableNewsRepository extends JpaRepository<SelectableNews, Integer>, JpaSpecificationExecutor<SelectableNews> {
 
 }
