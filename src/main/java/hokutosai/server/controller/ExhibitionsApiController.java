da4c5f92b354fca240588960313fabb1bbc43bbe
Serizawa Ryoji <i1111418.ryoji@gmail.com>
2016-04-17 17:28:53 +0900

    [add]exhibition_item関連のメソッドの追加

--- src/main/java/hokutosai/server/controller/ExhibitionsApiController.java
+++ src/main/java/hokutosai/server/controller/ExhibitionsApiController.java
@@ -1,12 +1,26 @@
 package hokutosai.server.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import hokutosai.server.data.entity.exhibitions.ExhibitionItem;
+import hokutosai.server.data.repository.exhibitions.ExhibitionItemRepository;
 
 @RestController
 @EnableAutoConfiguration
 @RequestMapping("/exhibitions")
 public class ExhibitionsApiController {
 
+	@Autowired
+	private ExhibitionItemRepository exhibitionItemRepository;
+
+	@RequestMapping(value = "/enumeration", method = RequestMethod.GET)
+	public List<ExhibitionItem> getEnumeration() {
+		return this.exhibitionItemRepository.findAll();
+	}
 }
