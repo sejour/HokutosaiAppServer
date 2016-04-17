1c19bd479c094e5d28d381970c9a8b42e4f2ac68
Serizawa Ryoji <i1111418.ryoji@gmail.com>
2016-04-17 20:37:41 +0900

    [add] SimpleExhibition.javaなどを追加

--- src/main/java/hokutosai/server/controller/ExhibitionsApiController.java
+++ src/main/java/hokutosai/server/controller/ExhibitionsApiController.java
@@ -1,6 +1,10 @@
 package hokutosai.server.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -9,7 +13,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import hokutosai.server.data.entity.exhibitions.ExhibitionItem;
+import hokutosai.server.data.entity.exhibitions.ExhibitionLike;
+import hokutosai.server.data.entity.exhibitions.SimpleExhibition;
+import hokutosai.server.data.json.account.AuthorizedAccount;
 import hokutosai.server.data.repository.exhibitions.ExhibitionItemRepository;
+import hokutosai.server.data.repository.exhibitions.ExhibitionLikeRepository;
+import hokutosai.server.data.repository.exhibitions.SimpleExhibitionRepository;
+import hokutosai.server.util.RequestAttribute;
 
 @RestController
 @EnableAutoConfiguration
@@ -19,8 +29,34 @@ public class ExhibitionsApiController {
 	@Autowired
 	private ExhibitionItemRepository exhibitionItemRepository;
 
+	@Autowired
+    private SimpleExhibitionRepository simpleExhibitionRepository;
+
+	@Autowired
+	private ExhibitionLikeRepository exhibitionLikeRepository;
+
 	@RequestMapping(value = "/enumeration", method = RequestMethod.GET)
 	public List<ExhibitionItem> getEnumeration() {
 		return this.exhibitionItemRepository.findAll();
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<SimpleExhibition> get(ServletRequest request) {
+	    List<SimpleExhibition> results = this.simpleExhibitionRepository.findAll();
+
+	    AuthorizedAccount account = RequestAttribute.getAccount(request);
+		if (account == null) return results;
+
+	    List<ExhibitionLike> likes = this.exhibitionLikeRepository.findByAccountId(account.getId());
+	    Map<Integer,ExhibitionLike> likesMap = new HashMap<Integer, ExhibitionLike>();
+
+	    for (ExhibitionLike like: likes) {
+	    	likesMap.put(like.getExhibitionId(), like);
+	    }
+	    for (SimpleExhibition shop: results) {
+	    	shop.setLiked(likesMap.containsKey(shop.getExhibitionId()));
+	    }
+
+	    return results;
 	}
 }
---
+++ src/main/java/hokutosai/server/data/entity/exhibitions/ExhibitionLike.java
@@ -0,0 +1,25 @@
+package hokutosai.server.data.entity.exhibitions;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import hokutosai.server.data.entity.Like;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "exhibitions_likes")
+public class ExhibitionLike  extends Like{
+
+	@Column(name = "exhibition_id")
+	@Getter @Setter
+	private Integer exhibitionId;
+
+	public ExhibitionLike() {}
+
+	public ExhibitionLike(Integer shopId, String accountId) {
+		super(accountId);
+		this.exhibitionId = shopId;
+	}
+}
