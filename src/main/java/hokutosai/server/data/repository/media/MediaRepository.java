c3483fae8a8449994be3844bc5fd2a0e4ed53b48
Shuka Takakuma <shuka.takakuma@gmail.com>
2016-04-18 16:48:36 +0900

    [add] newsIdを指定してメディアを削除するクエリを定義

--- src/main/java/hokutosai/server/data/repository/media/MediaRepository.java
+++ src/main/java/hokutosai/server/data/repository/media/MediaRepository.java
@@ -16,4 +16,9 @@ public interface MediaRepository extends JpaRepository<Media, String> {
 	@Query("UPDATE Media m SET m.newsId = :newsId WHERE m.mediaId = :mediaId")
 	public void link(@Param("mediaId") String mediaId, @Param("newsId") Integer newsId);
 
+	@Modifying
+    @Transactional
+    @Query("DELETE FROM Media m WHERE m.newsId = :newsId")
+	public void deleteByNewsId(@Param("newsId") Integer newsId);
+	
 }
