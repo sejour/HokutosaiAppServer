655e5f6fd96d5c5de1158f903c363de6ea76d53f
Shuka Takakuma <shuka.takakuma@gmail.com>
2016-04-18 15:51:38 +0900

    [implement] StatusResponseのコンストラクタを実装

--- src/main/java/hokutosai/server/data/json/StatusResponse.java
+++ src/main/java/hokutosai/server/data/json/StatusResponse.java
@@ -11,7 +11,15 @@ public class StatusResponse {
 	private Integer code;
 	
 	public StatusResponse(Integer code) {
-		
+		this.code = code;
+	}
+	
+	public StatusResponse(org.springframework.http.HttpStatus status) {
+		this.code = status.value();
+	}
+	
+	public StatusResponse(hokutosai.server.data.document.log.HttpStatus status) {
+		this.code = status.getCode();
 	}
 	
 }
