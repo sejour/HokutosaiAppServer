2f7e934e697ebc7729bd7d27f406b4da65f44144
sejour <shuka.takakuma@gmail.com>
2016-04-19 23:14:12 +0900

    [delete] TopicLink entity and repository

--- src/main/java/hokutosai/server/data/repository/news/TopicLinkRepository.java
+++
@@ -1,9 +0,0 @@
-package hokutosai.server.data.repository.news;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import hokutosai.server.data.entity.news.TopicLink;
-
-public interface TopicLinkRepository extends JpaRepository<TopicLink, Integer> {
-
-}
