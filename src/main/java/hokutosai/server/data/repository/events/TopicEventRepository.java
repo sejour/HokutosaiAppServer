c35d20c2e79b51ceb69560f875042bf2451eebe7
sejour <shuka.takakuma@gmail.com>
2016-04-20 00:41:22 +0900

    [add] 日時を指定してその時間でアクティブなイベントを全件取得するクエリを追加

--- src/main/java/hokutosai/server/data/repository/events/TopicEventRepository.java
+++ src/main/java/hokutosai/server/data/repository/events/TopicEventRepository.java
@@ -1,9 +1,11 @@
 package hokutosai.server.data.repository.events;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import hokutosai.server.data.entity.events.TopicEvent;
 
@@ -11,5 +13,8 @@ public interface TopicEventRepository extends JpaRepository<TopicEvent, Integer>
 
 	@Query("SELECT t FROM TopicEvent t WHERE t.featured = true")
 	public List<TopicEvent> findFeaturedAll();
+
+	@Query("SELECT t FROM TopicEvent t WHERE t.date = :datetime AND t.startTime <= :datetime AND t.endTime > :datetime")
+	public List<TopicEvent> findDatetimeActiveAll(@Param("datetime") Date datetime);
 
 }
