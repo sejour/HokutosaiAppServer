98dc88019e835f9dc476cae62af087bf8fcecefd
sejour <shuka.takakuma@gmail.com>
2016-04-19 23:10:27 +0900

    [delete] getLink()メソッドを削除
    リンク付きトピックを廃止するため

--- src/main/java/hokutosai/server/data/json/news/Topic.java
+++ src/main/java/hokutosai/server/data/json/news/Topic.java
@@ -3,20 +3,17 @@ package hokutosai.server.data.json.news;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public interface Topic {
-	
+
 	@JsonProperty("title")
 	public String getTitle();
-	
+
 	@JsonProperty("media_url")
 	public String getMediaUrl();
-	
-	@JsonProperty("link")
-	public String getLink();
-	
+
 	@JsonProperty("event_id")
 	public Integer getEventId();
-	
+
 	@JsonProperty("news_id")
 	public Integer getNewsId();
-	
+
 }
