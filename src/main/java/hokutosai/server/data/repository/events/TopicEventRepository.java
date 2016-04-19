1d085a7cfd8d9380b33c61c24452779a6cc1fb58
sejour <shuka.takakuma@gmail.com>
2016-04-20 00:21:26 +0900

    [add] 注目のイベントを全て取得するクエリを追加

--- src/main/java/hokutosai/server/data/repository/events/TopicEventRepository.java
+++ src/main/java/hokutosai/server/data/repository/events/TopicEventRepository.java
@@ -1,9 +1,15 @@
 package hokutosai.server.data.repository.events;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import hokutosai.server.data.entity.events.TopicEvent;
 
 public interface TopicEventRepository extends JpaRepository<TopicEvent, Integer> {
-	
+
+	@Query("SELECT t FROM TopicEvent t WHERE t.featured = true")
+	public List<TopicEvent> findFeaturedAll();
+
 }
