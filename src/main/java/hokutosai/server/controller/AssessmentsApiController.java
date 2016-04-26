0f29a64b840ce4e6afa9295e48a341dda482d7f0
sejour <shuka.takakuma@gmail.com>
2016-04-26 23:26:39 +0900

    [implement] GET /assessments/reports/causes

--- src/main/java/hokutosai/server/controller/AssessmentsApiController.java
+++ src/main/java/hokutosai/server/controller/AssessmentsApiController.java
@@ -1,7 +1,15 @@
 package hokutosai.server.controller;
 
+import hokutosai.server.data.entity.assessments.AssessmentReportCause;
+import hokutosai.server.data.repository.assessments.AssessmentReportCauseRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/assessments")
 public class AssessmentsApiController {
 
+	@Autowired
+	private AssessmentReportCauseRepository assessmentReportCauseRepository;
 
-
+	@RequestMapping(value = "/reports/causes", method = RequestMethod.GET)
+	public List<AssessmentReportCause> getReportsCauses() {
+		return this.assessmentReportCauseRepository.findAll(new Sort(Sort.Direction.DESC, "sequence"));
+	}
 }
