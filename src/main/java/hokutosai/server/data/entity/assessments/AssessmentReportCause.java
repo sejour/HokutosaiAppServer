c5fced95f5e4197a6c8d2eeec0e38849a9a76f9a
sejour <shuka.takakuma@gmail.com>
2016-04-26 23:14:31 +0900

    [add] AssessmentReportCause entity

---
+++ src/main/java/hokutosai/server/data/entity/assessments/AssessmentReportCause.java
@@ -0,0 +1,28 @@
+package hokutosai.server.data.entity.assessments;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Entity
+@Table(name = "reports_causes")
+@Data
+public class AssessmentReportCause {
+
+	@Id
+	@Column(name = "cause_id") @GeneratedValue(strategy = GenerationType.AUTO)
+	@JsonProperty("cause_id")
+	private Integer cuaseId;
+
+	@Column(name = "text", nullable = false)
+	@JsonProperty("text")
+	private String text;
+
+}
