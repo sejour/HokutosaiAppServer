f0a1e3a2d6dcc779cb7a9a2aacf6f940f06114ba
sejour <shuka.takakuma@gmail.com>
2016-04-29 01:12:55 +0900

    [fix] 不要なspecificationを削除

--- src/main/java/hokutosai/server/data/specification/EventSpecifications.java
+++ src/main/java/hokutosai/server/data/specification/EventSpecifications.java
@@ -1,7 +1,6 @@
 package hokutosai.server.data.specification;
 
 import java.sql.Date;
-import java.sql.Time;
 
 import org.springframework.data.jpa.domain.Specification;
 
@@ -9,31 +8,13 @@ import hokutosai.server.data.entity.events.SimpleEvent;
 
 public class EventSpecifications {
 
-	public static Specification<SimpleEvent> equalSimpleEventDate(Date date) {
+	public static Specification<SimpleEvent> filterByDate(Date date) {
 		return date == null ? null : (root, query, cb) -> {
             return cb.equal(root.<Date>get("date"), date);
         };
     }
 
-	public static Specification<SimpleEvent> equalSimpleEventDate(java.util.Date date) {
-		return date == null ? null : (root, query, cb) -> {
-            return cb.equal(root.<Date>get("date"), date);
-        };
-    }
-
-	public static Specification<SimpleEvent> laterThanEndtime(Time nowDatetime) {
-		return nowDatetime == null ? null : (root, query, cb) -> {
-            return cb.greaterThan(root.<Time>get("endTime"), nowDatetime);
-        };
-    }
-
-	public static Specification<SimpleEvent> earlierThanStarttime(Time nowDatetime) {
-		return nowDatetime == null ? null : (root, query, cb) -> {
-            return cb.lessThanOrEqualTo(root.<Time>get("startTime"), nowDatetime);
-        };
-    }
-
-	public static Specification<SimpleEvent> filterBySimpleEventPlaceId(Integer placeId) {
+	public static Specification<SimpleEvent> filterByPlaceId(Integer placeId) {
 		return placeId == null ? null : (root, query, cb) -> {
             return cb.equal(root.get("place").get("placeId"), placeId);
         };
