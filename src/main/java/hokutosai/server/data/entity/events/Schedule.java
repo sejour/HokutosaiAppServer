5c63722f356483e30e0c42da2098c524179517c7
sejour <shuka.takakuma@gmail.com>
2016-05-03 16:19:25 +0900

    [add] Schedule entity

---
+++ src/main/java/hokutosai/server/data/entity/events/Schedule.java
@@ -0,0 +1,43 @@
+package hokutosai.server.data.entity.events;
+
+import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Entity
+@Table(name = "schedules")
+@Data
+public class Schedule {
+
+	@Id
+	@Column(name = "id")
+	@JsonProperty("schedule_id")
+	private Integer scheduleId;
+
+	@Column(name = "date", nullable = false)
+	@JsonProperty("date")
+	private Date date;
+
+	@Column(name = "day", nullable = false)
+	@JsonProperty("day")
+	private String day;
+
+	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+	@JoinColumn(name = "date", insertable = false, updatable = false)
+	@OrderBy("startTime ASC")
+	@JsonProperty("timetable")
+	private List<SimpleEvent> timetable;
+
+}
