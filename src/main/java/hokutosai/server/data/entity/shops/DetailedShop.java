a716307246425347a4cb6de1cb7b2b47d1598f2d
sejour <shuka.takakuma@gmail.com>
2016-05-08 19:23:55 +0900

    [change] Shopにassessment関連を除く全ての情報を含める変更

--- src/main/java/hokutosai/server/data/entity/shops/DetailedShop.java
+++ src/main/java/hokutosai/server/data/entity/shops/DetailedShop.java
@@ -1,15 +1,10 @@
 package hokutosai.server.data.entity.shops;
 
-import hokutosai.server.data.entity.places.Place;
-
 import java.util.List;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
@@ -22,23 +17,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @Entity
 @Table(name = "shops")
 public class DetailedShop extends Shop {
-
-	@Column(name = "introduction")
-	@JsonProperty("introduction")
-	@Getter @Setter
-	private String introduction;
-
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "place_id", insertable = false, updatable = false)
-	@JsonProperty("place")
-	@Getter @Setter
-	private Place place;
-
-	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-	@JoinColumn(name = "shop_id", insertable = false, updatable = false)
-	@JsonProperty("menu")
-	@Getter @Setter
-	private List<MenuItem> menu;
 
 	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
 	@JoinColumn(name = "shop_id", insertable = false, updatable = false)
